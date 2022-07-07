a35e6aa1cbbb11ab6a685be9e4e9cc0d4dd4128a
talmawash <tariqalmawash@gmail.com>
2022-07-07 11:01:29 -0700

    Main view controller files init

---
+++ Sharecart/ViewControllers/MainViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainViewController.h
+//  Sharecart
+//
+//  Created by Tariq Almawash on 7/7/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
