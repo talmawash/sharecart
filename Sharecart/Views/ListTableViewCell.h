14a5c55362047c4b5bf5bdd9d76110925b02f49c
talmawash <tariqalmawash@gmail.com>
2022-07-08 15:51:42 -0700

    init list custom table view cell

---
+++ Sharecart/Views/ListTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  ListTableViewCell.h
+//  Sharecart
+//
+//  Created by Tariq Almawash on 7/8/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ListTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Sharecart/Views/ListTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ListTableViewCell.m
+//  Sharecart
+//
+//  Created by Tariq Almawash on 7/8/22.
+//
+
+#import "ListTableViewCell.h"
+
+@implementation ListTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
