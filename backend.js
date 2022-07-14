630f4375e56c3344c7e8b4adeda1cec241e97cdc
talmawash <tariqalmawash@gmail.com>
2022-07-14 13:01:31 -0700

    fix and clean up get lists backend code
    (cherry picked from commit 05e6daedcf37bb8bfdb66bdbf5130f5a62534ad6)

--- backend.js
+++ backend.js
@@ -1,19 +1,17 @@
 Parse.Cloud.define("getLists", async (request) => {
-  if (request.user) {
-    var mainQuery;
-    var relation = await request.user.get("lists", {useMasterKey: true});
-    var lists = await relation.query().find({useMasterKey: true});
-    for (var i = 0; i < lists.length; i++) {
-      var subQuery = new Parse.Query("SharecartList");
-      subQuery.equalTo("id", lists.id);
-      if (!mainQuery) {
-        mainQuery = subQuery;
-      } else {
-        mainQuery = Parse.Query.or(mainQuery, subQuery);
-      }
+  let mainQuery;
+  const relation = await request.user.get("lists");
+  const lists = await relation.query().find({ useMasterKey: true });
+  for (let i = 0; i < lists.length; i++) {
+    let subQuery = new Parse.Query("SharecartList");
+    subQuery.equalTo("objectId", lists[i]._getId());
+    if (!mainQuery) {
+      mainQuery = subQuery;
+    } else {
+      mainQuery = Parse.Query.or(mainQuery, subQuery);
     }
-    return await mainQuery.find({useMasterKey: true});
-  } else {
-    throw ("Must be signed in");
   }
+  return await mainQuery.find({ useMasterKey: true });
+},{
+  requireUser: true
 });
