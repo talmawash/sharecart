6a1dd8257be4f3bd706c201abb5fb5d5368e09ef
talmawash <tariqalmawash@gmail.com>
2022-07-14 14:22:30 -0700

    adding items is through cloud code now

--- Backend/items.js
+++ Backend/items.js
@@ -1,8 +1,8 @@
 Parse.Cloud.define("getItems", async (request) => {
   const relation = await request.user.get("lists");
   const list = await relation.query().equalTo("objectId", request.params.listId).find({ useMasterKey: true });
-  if (list.length == 1) {
-    let query = new Parse.Query("SharecartItem").equalTo("list", list[0]);
+  if (list.length == 1) { // User is indeed a member of that list
+    const query = new Parse.Query("SharecartItem").equalTo("list", list[0]);
     return await query.find({ useMasterKey: true });
   }
 },{
@@ -10,3 +10,17 @@ Parse.Cloud.define("getItems", async (request) => {
   requireUser: true
 });
 
+Parse.Cloud.define("newItem", async (request) => {
+  const relation = await request.user.get("lists");
+  const list = await relation.query().equalTo("objectId", request.params.listId).find({ useMasterKey: true });
+  if (list.length == 1) { // User is indeed a member of that list
+    const item = new Parse.Object("SharecartItem");
+    return await item.save({
+      name: request.params.name,
+      list: list[0]
+    }, { useMasterKey: true });
+  }
+},{
+  fields: ["name", "listId"],
+  requireUser: true
+});
