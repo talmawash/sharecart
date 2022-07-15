fa27bce5ca91b03d6151838b9ec2923e161ad5a3
talmawash <tariqalmawash@gmail.com>
2022-07-15 15:28:26 -0700

    remove unnecessary code

--- Backend/lists.js
+++ Backend/lists.js
@@ -1,21 +1,7 @@
 Parse.Cloud.define("getLists", async (request) => {
   let mainQuery;
   const relation = await request.user.get("lists");
-  const lists = await relation.query().find({ useMasterKey: true });
-  for (let i = 0; i < lists.length; i++) {
-    let subQuery = new Parse.Query("SharecartList");
-    subQuery.equalTo("objectId", lists[i]._getId());
-    if (!mainQuery) {
-      mainQuery = subQuery;
-    } else {
-      mainQuery = Parse.Query.or(mainQuery, subQuery);
-    }
-  }
-  if (mainQuery) {
-    return await mainQuery.find({ useMasterKey: true });
-  } else {
-    return { };
-  }
+  return await relation.query().find({ useMasterKey: true });
 },{
   requireUser: true
 });
