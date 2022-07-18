59895d252fcde506095184365f191e4f32871664
talmawash <63553192+talmawash@users.noreply.github.com>
2022-07-18 13:34:13 -0700

    Update auth.js

--- Backend/auth.js
+++ Backend/auth.js
@@ -2,8 +2,7 @@ Parse.Cloud.define("signup", async (request) => {
     let user = new Parse.User();
     user.set("username", request.params.username);
     user.set("password", request.params.password);
-    await user.signUp(null, { useMasterKey: true });
-    return user; // This will be reached if the above operations don't throw exceptions.
+    return await user.signUp(null, { useMasterKey: true });
 },{
   fields: ["username", "password"],
   requireUser: false
