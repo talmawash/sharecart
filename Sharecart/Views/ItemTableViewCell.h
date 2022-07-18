eeb4e4cef5a170451f77435019d7b9b16c018db8
talmawash <tariqalmawash@gmail.com>
2022-07-18 16:32:22 -0700

    init item custom view cell, to be used later

---
+++ Sharecart/Views/ItemTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  ItemTableViewCell.h
+//  Sharecart
+//
+//  Created by Tariq Almawash on 7/18/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ItemTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
