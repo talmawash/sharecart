47e644e52b3fa43d2e16846797042821428d9352
talmawash <tariqalmawash@gmail.com>
2022-07-20 15:29:03 -0700

    client side create invitation

--- Sharecart/ViewControllers/ListViewController.m
+++ Sharecart/ViewControllers/ListViewController.m
@@ -75,7 +75,14 @@
 }
 
 - (IBAction)inviteTap:(id)sender {
-    
+    [PFCloud callFunctionInBackground:@"createInvitation" withParameters:@{@"listId": self.list.objectId} block:^(PFObject *object, NSError * _Nullable error) {
+        if (!error) {
+            [SwiftAdapter displayInvitationCodeWithCode:object.objectId];
+        }
+        else {
+            // TODO: Prompt user to check connection and try again
+        }
+    }];
 }
 
 #pragma mark - Table View
