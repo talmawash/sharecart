896a895343d6dec4d7c7fe32273a34f8009ce9da
talmawash <tariqalmawash@gmail.com>
2022-07-20 12:42:18 -0700

    add adapter that can be used to call swift code

---
+++ Sharecart/SwiftAdapter.swift
@@ -0,0 +1,14 @@
+//
+//  SwiftAdapter.swift
+//  Sharecart
+//
+//  Created by Tariq Almawash on 7/19/22.
+//
+
+import Foundation
+import SwiftEntryKit
+import UIKit
+
+public class SwiftAdapter : NSObject {
+
+}
